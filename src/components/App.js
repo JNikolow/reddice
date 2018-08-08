d646f60b89c80f4b5866f15f2cb694110a0ad76d
Joro <joro.nikolow@gmail.com>
2018-08-08 14:43:56 +0300

    Greetings

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Greetings from './Greetings'
+
+class App extends Component {
+  render() {
+    return (
+      <Greetings />
+    );
+  }
+}
+
+export default App;
